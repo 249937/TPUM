fdb5d767630f1763a1ec8f8e0599f0811c9e564f
Maciej Włodarczyk <maciejwlodarczyk15@gmail.com>
2023-03-22 19:44:57 +0100

    Replaced ShopService interface with abstract class

---
+++ TPUM.Logic/ShopServiceAbstract.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using TPUM.Data;
+
+namespace TPUM.Logic
+{
+    internal abstract class ShopServiceAbstract
+    {
+        private class ShopService : ShopServiceAbstract
+        {
+            private readonly IProductRepository _productRepository;
+
+
+            public ShopService()
+            {
+                _productRepository = null;
+            }
+
+            public void AddProduct(string name, float price)
+            {
+                if (name == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    throw new ArgumentException();
+                }
+                if (price <= 0.0f)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                Product product = new Product(Guid.NewGuid());
+                product.SetName(name);
+                product.SetPrice(price);
+
+                _productRepository.Add(product);
+            }
+
+            public void RemoveProduct(Guid productGuid)
+            {
+                if (Guid.Empty.Equals(productGuid))
+                {
+                    throw new ArgumentException();
+                }
+                _productRepository.Remove(productGuid);
+            }
+        }
+    }
+}
