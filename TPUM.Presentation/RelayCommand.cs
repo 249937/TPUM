5807cec62b786fff5e196d3845163b5cd5acd2b4
Maciej Włodarczyk <maciejwlodarczyk15@gmail.com>
2023-03-23 00:56:54 +0100

    Added RelayCommand

---
+++ TPUM.Presentation/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TPUM.Presentation
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action executeAction;
+        private readonly Func<bool> canExecute;
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            executeAction = execute ?? throw new ArgumentNullException();
+            this.canExecute = canExecute;
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            executeAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return canExecute();
+            }
+        }
+
+        internal void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
