ba2b9f448273bba24eda5f72c337658085b41c31
Maciej Włodarczyk <maciejwlodarczyk15@gmail.com>
2023-03-23 01:38:32 +0100

    Added User Interface in Presentation Layer

--- TPUM.Presentation/ViewModel.cs
+++ TPUM.Presentation/ViewModel.cs
@@ -1,12 +1,119 @@
-﻿namespace TPUM.Presentation
-{
-    internal class ViewModel
-    {
-        private Model MyModel { get; set; }
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-        internal ViewModel(Model model = default(Model))
+namespace TPUM.Presentation
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public RelayCommand CommandAddProduct 
+        { 
+            get; 
+            private set; 
+        }
+        public RelayCommand CommandRemoveProduct 
+        { 
+            get; 
+            private set; 
+        }
+        public RelayCommand CommandFindProduct 
+        { 
+            get; 
+            private set; 
+        }
+
+        private string productNameInputText;
+        public string ProductNameInputText
         {
-            MyModel = model ?? new Model();
+            get 
+            { 
+                return productNameInputText; 
+            }
+            set 
+            {
+                SetProperty(ref productNameInputText, value);
+            }
+        }
+
+        private string productPriceInputText;
+        public string ProductPriceInputText
+        {
+            get 
+            { 
+                return productPriceInputText; 
+            }
+            set 
+            { 
+                SetProperty(ref productPriceInputText, value);
+            }
+        }
+
+        private string productGuidInputText;
+        public string ProductGuidInputText
+        {
+            get 
+            { 
+                return productGuidInputText; 
+            }
+            set 
+            {
+                SetProperty(ref productGuidInputText, value);
+            }
+        }
+
+        private string outputText;
+        public string OutputText
+        {
+            get 
+            { 
+                return outputText; 
+            }
+            set 
+            { 
+                SetProperty(ref outputText, value); 
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            OnPropertyChaned(propertyName);
+            return true;
+        }
+
+        private void OnPropertyChaned(string propertyName)
+        {
+            var eventHandler = PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        
+        public ViewModel()
+        {
+            CommandAddProduct = new RelayCommand(ExecuteCommandAddProduct);
+            CommandRemoveProduct = new RelayCommand(ExecuteCommandRemoveProduct);
+            CommandFindProduct = new RelayCommand(ExecuteCommandFindProduct);
+        }
+        private void ExecuteCommandAddProduct()
+        {
+            OutputText = "[Product] Name: " + ProductNameInputText + ", [Price]: " + ProductPriceInputText + " ADDED";
+        }
+
+        private void ExecuteCommandRemoveProduct()
+        {
+            OutputText = "[Product] GUID: " + ProductGuidInputText + " REMOVED";
+        }
+
+        private void ExecuteCommandFindProduct()
+        {
+            OutputText = "[Product] Name: " + ProductNameInputText + " FOUND";
         }
     }
 }
